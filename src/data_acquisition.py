59d86926b4682e74d5cd23dbe663d83305dc43c7
florent-haffner <florent.haffner@protonmail.com>
2021-03-14 00:28:03 +0100

    data_acquisition.py -> added data preprocessing before working /w model

--- src/data_acquisition.py
+++ src/data_acquisition.py
@@ -1,6 +1,8 @@
 from requests import get
+
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 __KRAKEN_API = "https://api.kraken.com/0/public/OHLC"
 
@@ -44,22 +46,53 @@ def getDataframe(apiResponse) -> pd.DataFrame:
     return df
 
 
+"""
+Glue everything together
+"""
 def getFormattedData(asset, interval) -> pd.DataFrame:
     print('Quering ' + asset + '\'s on interval ' + interval + 'min from Kraken')
     datas = getDatasFromKraken(asset, interval)
     return getDataframe(datas)
 
 
-def splitDataset(full_dataset, split_ratio):
+"""
+Help split dataset
+:param full_dataset -> straight forward
+:param split_radio -> value between 0 & 1, like 0.85 for example
+:return properly splitted train_dataset and test_dataset
+"""
+def splitDataset(full_dataset, split_ratio) -> [pd.DataFrame]:
     split_length = int(len(full_dataset) * split_ratio)
     train_set = full_dataset[:split_length]
     test_set = full_dataset[split_length:]
     return train_set, test_set
 
+
+def getXy(datas, X_key) -> [pd.DataFrame]:
+    X = datas[X_key]
+    y = datas['timestamp']
+    return X, y
+
+
+"""
+:param get X, y
+:return scaled dataset from 0 to 1
+"""
+def process_data_to_scale(dataset) -> MinMaxScaler:
+    return scaler.fit_transform(
+        dataset.values.reshape(len(dataset.values), 1)
+    )
+
+
 if __name__ == "__main__":
-    # df = getFormattedData('BTCEUR', '1')
-    # df.plot('timestamp', 'volume')
+    df = getFormattedData('BTCEUR', '60')
+    df.plot('timestamp', 'volume')
     # plt.show()
 
-    train, test = splitDataset([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], .85)
-    print(train, test)
+    train, test = splitDataset(df, .85)
+    X, y = getXy(train, 'volume')
+
+    # Pre-process data to scale to 0->1
+    scaler = MinMaxScaler()
+    X_train = process_data_to_scale(X)
+    print(X_train)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 import matplotlib.pyplot as plt
-from data_acquisition import getFormattedData
+from sklearn.preprocessing import MinMaxScaler
+from data_acquisition import getFormattedData, splitDataset
 
 
 if __name__ == "__main__":
-    minute = getFormattedData('BTCEUR', '1')
-    print(len(minute))
-    minute.plot('timestamp', 'volume')
-    plt.show()
-
+    # minute = getFormattedData('BTCEUR', '1')
+    # print(len(minute))
+    # minute.plot('timestamp', 'volume')
+    # plt.show()
+    #
     heure = getFormattedData('BTCEUR', '60')
+    # print(len(heure))
+    # heure.plot('timestamp', 'volume')
+    # plt.show()
+    #
     print(len(heure))
-    heure.plot('timestamp', 'volume')
-    plt.show()
+    train, test = splitDataset(heure, 0.85)
+    print(len(train), len(test))
+
+
+    min_max_scaler = MinMaxScaler()
