2bc9a90551abfaa1828ca736b8da213cf90ced53
florent-haffner <florent.haffner@protonmail.com>
2021-03-27 15:15:36 +0100

    main.py -> simplified and used smaller dataset for 1-6h analysis

--- src/main.py
+++ src/main.py
@@ -1,78 +1,34 @@
-import json
-from data_acquisition import getFormattedData
+from datetime import datetime
 
-import tensorflow as tf
-from src.stocks_indicators import get_stocks_indicator
 import matplotlib.pyplot as plt
+
+from data_acquisition import getFormattedData
+from stocks_indicators import get_stocks_indicator
+
 
 def get_measure_viz(df, measure):
     df[measure].plot()
     plt.title(measure)
     plt.show()
-    # print(measure)
-    # print(df[measure])
 
 
 def get_last_n_percentage(df, nbr_percentage):
-    print(len(df))
     calculated_length = (len(df) * nbr_percentage) / 100
-    tmp = df[:int(calculated_length)]
-    print(len(tmp))
-    return tmp
-
-
-def run():
-    # #
-    # # TODO -> TF CONFIG : seems like it help my model to get over the training process
-    # #
-    # config = tf.compat.v1.ConfigProto()
-    # config.gpu_options.per_process_gpu_memory_fraction = 0.7
-    # tf.compat.v1.keras.backend.set_session(
-    #     tf.compat.v1.Session(config=config))
-    #
-    # model = None
-    # with open("ressources/applications.properties.json", 'r') as applications_properties:
-    #     properties = json.load(applications_properties)
-
-    asset = 'ETHEUR'
-    interval = '60'
-
-    df = getFormattedData(asset, interval)
-    df.plot('timestamp', 'volume')
-    plt.title('timestamp vs volume')
-    plt.show()
-    return df
-    # plt.show()
-
-        # train, test = splitDataset(df, .85)
-        # X, y = getXy(dataset=train, X_key='timestamp', y_key='volume')
-        #
-        # # Pre-process data to scale to 0->1
-        # scaler = MinMaxScaler()
-        # X_train, y_train = process_data_to_scale(scaler, X, y)
-        #
-        # # TODO : if needed to inverse "scaling"
-        # # inverse = scaler.inverse_transform(y_train)
-        #
-        # model = Model(properties)
-        #
-        # print('[MODEL] PROCESS INPUT TO ENABLE USAGE OF LSTM')
-        # X_train = model.process_input(properties['model']['layers'][0]['input_timesteps'], X_train)
-        # y_train = model.process_input(properties['model']['layers'][0]['input_timesteps'], y_train)
-        # # model.train(properties, X_train, y_train)
+    tmp_df = df[:int(calculated_length)]
+    print('TMP_DF - from', datetime.fromtimestamp(tmp_df.head(1)['timestamp'].iloc[0]),
+          'to', datetime.fromtimestamp(tmp_df.tail(1)['timestamp'].iloc[-1]))
+    return tmp_df
 
 
 if __name__ == "__main__":
-    df = run()
-    # df = [0,1,2,3,4,5,6,7,8,9]
+    asset = 'GRTEUR'
+    interval = '60'
+    df = getFormattedData(asset, interval)
 
-    print('Dataset :', len(df) / 24, 'days')
     df = get_stocks_indicator(df)
     print(df.keys())
 
     tmp_df = get_last_n_percentage(df, 25)
-    tmp_df.head(1)
-    tmp_df.tail(1)
 
     get_measure_viz(tmp_df, 'close_12_ema')
     get_measure_viz(tmp_df, 'close_26_ema')
--- src/model.py
+++ src/model.py
@@ -1,9 +1,12 @@
 import os
 from datetime import datetime
 
+from matplotlib import pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.python.keras.callbacks import ModelCheckpoint
+
+from src.data_acquisition import getFormattedData
 
 
 class Model:
@@ -80,3 +83,44 @@ class Model:
     #         workers=config['model']['training']['workers']
     #     )
     #     print('[MODEL] finished training process')
+
+
+def run():
+    # #
+    # # TODO -> TF CONFIG : seems like it help my model to get over the training process
+    # #
+    # config = tf.compat.v1.ConfigProto()
+    # config.gpu_options.per_process_gpu_memory_fraction = 0.7
+    # tf.compat.v1.keras.backend.set_session(
+    #     tf.compat.v1.Session(config=config))
+    #
+    # model = None
+    # with open("ressources/applications.properties.json", 'r') as applications_properties:
+    #     properties = json.load(applications_properties)
+
+    asset = 'ETHEUR'
+    interval = '60'
+
+    df = getFormattedData(asset, interval)
+    df.plot('timestamp', 'volume')
+    plt.title('timestamp vs volume')
+    plt.show()
+    return df
+    # plt.show()
+
+        # train, test = splitDataset(df, .85)
+        # X, y = getXy(dataset=train, X_key='timestamp', y_key='volume')
+        #
+        # # Pre-process data to scale to 0->1
+        # scaler = MinMaxScaler()
+        # X_train, y_train = process_data_to_scale(scaler, X, y)
+        #
+        # # TODO : if needed to inverse "scaling"
+        # # inverse = scaler.inverse_transform(y_train)
+        #
+        # model = Model(properties)
+        #
+        # print('[MODEL] PROCESS INPUT TO ENABLE USAGE OF LSTM')
+        # X_train = model.process_input(properties['model']['layers'][0]['input_timesteps'], X_train)
+        # y_train = model.process_input(properties['model']['layers'][0]['input_timesteps'], y_train)
+        # # model.train(properties, X_train, y_train)
