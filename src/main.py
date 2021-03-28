f613cab48a3434abc8cc7c2b47b31136576065b8
florent-haffner <florent.haffner@protonmail.com>
2021-03-28 17:22:06 +0200

    main.py -> update trend calculation : able to detect last curve and add it to a list. Next step -> detect a trend on three days (volume + price)

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 from data_acquisition import getFormattedData
-from stocks_indicators import get_stocks_indicator
+from stocks_indicators import get_stocks_indicators
 
 
 def get_measure_viz(df, measure):
@@ -20,42 +20,45 @@ def get_last_n_percentage(df, nbr_percentage):
     return tmp_df
 
 
-def detect_last_recent_top(df):
-    print('Comparing current macds to previous one')
+def detect_top_bottom(df):
+    print('CURVE - Detecting tendancy')
+    tendency = {}
+    print(df['macds'][len(df)-1])
     for n in range(len(df) - 1, 0, -1):
         first_index = None
-        top_index = None
-        if (df['macds'][n] > df['macds'][n - 1]):
+        if df['macds'][n] > df['macds'][n - 1]:
             if first_index is None:
                 first_index = n
-            top_index = n
-            if top_index > first_index-5:
-                print('Found 5 concurrent value - Curve to detected')
-                print('Last recent top found at', datetime.fromtimestamp(df['timestamp'][top_index]))
+            last_index = n
+            if last_index > first_index - 5:
+                print('Found 5 concurrent value')
+                timestamp = str(datetime.fromtimestamp(df['timestamp'][last_index]))
+                price = df['close_26_ema'][last_index]
+
+                tendency[first_index] = {"top": {"datetime": timestamp, "price": price}}
+                print(tendency)
             return first_index
 
-        print('TODO - Understand trend while curve is currently going up')
+    print('TODO - Understand trend while curve is currently going up')
 
 
-def high_valatility_volume_analysis(df, short_df):
+def high_volatility_volume_analysis(df, short_df):
     print('\n[TREND ANALYSIS]')
-    top_index = detect_last_recent_top(short_df)
-    print(top_index)
+    tendency = detect_top_bottom(short_df)
+    print(tendency)
 
 
 if __name__ == "__main__":
     asset = 'GRTEUR'
     interval = '60'
     df = getFormattedData(asset, interval)
+    df_with_indicators = get_stocks_indicators(df)
+    three_day_DF = get_last_n_percentage(df_with_indicators, 25)
 
-    df = get_stocks_indicator(df)
-    tmp_df = get_last_n_percentage(df, 25)
+    # get_measure_viz(tmp_df, 'close_12_ema')
+    get_measure_viz(three_day_DF, 'close_26_ema')
+    get_measure_viz(three_day_DF, 'macds')
+    # get_measure_viz(tmp_df, 'macd')
+    # get_measure_viz(tmp_df, 'macdh')
 
-    get_measure_viz(tmp_df, 'close_12_ema')
-    get_measure_viz(tmp_df, 'close_26_ema')
-
-    get_measure_viz(tmp_df, 'macd')
-    get_measure_viz(tmp_df, 'macds')
-    get_measure_viz(tmp_df, 'macdh')
-
-    high_valatility_volume_analysis(df, tmp_df)
+    high_volatility_volume_analysis(df_with_indicators, three_day_DF)
--- src/stocks_indicators.py
+++ src/stocks_indicators.py
@@ -1,7 +1,7 @@
 from stockstats import StockDataFrame
 
 
-def get_stocks_indicator(df):
+def get_stocks_indicators(df):
     df = StockDataFrame.retype(df)
     df['macd'] = df.get('macd')  # calculate MACD
     df['close_12_ema'] = df.get('close_12_ema')  # calculate MACD
