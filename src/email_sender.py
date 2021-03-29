40ba5e35012bb3cbc2cdaf0fdb28b067a7abc9cd
florent-haffner <florent.haffner@protonmail.com>
2021-03-29 22:53:41 +0200

    email_sender.py -> create a function that send email + update .env

---
+++ src/email_sender.py
@@ -0,0 +1,32 @@
+import os
+import smtplib
+from dotenv import load_dotenv
+
+load_dotenv()
+
+__EMAIL_USER = os.getenv('email_sender')
+__EMAIL_PASSWORD = os.getenv('email_password')
+
+
+def send_email(destination, subject, text):
+    SMTPServer = smtplib.SMTP("smtp.gmail.com", 587)
+    SMTPServer.ehlo()
+    SMTPServer.starttls()
+    SMTPServer.ehlo()
+    SMTPServer.login(__EMAIL_USER, __EMAIL_PASSWORD)
+
+    header = 'To:' + destination + '\n' +\
+             'From: ' + __EMAIL_USER + '\n' +\
+             'Subject:' + subject + '\n'
+
+    msg = header + '\n' + text
+    print(header)
+
+    SMTPServer.sendmail(__EMAIL_USER, destination, msg)
+    print('Sended a ', len(text), ' characers email')
+    SMTPServer.close()
+
+
+if __name__ == '__main__':
+    destination = 'neltharak@gmail.com'
+    send_email(destination, 'Hello world!', 'Incoming news :D')
