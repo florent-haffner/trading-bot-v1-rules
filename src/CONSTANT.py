d5b3e6061832afe7703f101b712bb423c92cd88a
florent-haffner <florent.haffner@protonmail.com>
2021-04-03 23:40:05 +0200

    mission_service.py + CONSTANT.py -> evolution to integrate bot /w MongoBD

--- src/CONSTANT.py
+++ src/CONSTANT.py
@@ -1,3 +1,8 @@
 
 __EMAIL_USER = "florent.haffner@gmail.com"
 __EMAIL_PASSWORD = "tZSSCpNrzB7xq6ViDwcT3KFU2H4Y"
+
+__MONGO_HOST = "192.168.1.58"
+__MONGO_PORT = 27017
+__MONGO_USER = "mongoroot"
+__MONGO_PASSWORD = "f$8A#@f3uYgU^o"
---
+++ src/mission_service.py
@@ -0,0 +1,58 @@
+from pymongo import MongoClient
+from CONSTANT import __MONGO_HOST, __MONGO_PORT, __MONGO_USER, __MONGO_PASSWORD
+
+# __MONGO_URI = 'mongodb://' + \
+#     __MONGO_USER + ":" + \
+#     "\"" +__MONGO_PASSWORD + "\"@" + \
+#     __MONGO_HOST + ':' + \
+#     str(__MONGO_PORT)
+# print(__MONGO_URI)
+
+# __MONGO_CLIENT = MongoClient(__MONGO_URI)
+
+# __MONGO_CLIENT = MongoClient(__MONGO_HOST,
+#                              username=__MONGO_USER,
+#                              password=__MONGO_PASSWORD,
+#                              authSource="the_database",
+#                              authMecanism="SCRAM-SHA-1")
+
+__MONGO_CLIENT = MongoClient(__MONGO_HOST,
+                             username=__MONGO_USER,
+                             password=__MONGO_PASSWORD)
+
+db = __MONGO_CLIENT.test_database
+collection = db.mission
+
+
+def createMission(data):
+    print('[NEW MISSION] ->', data)
+    collection.insert_one(data)
+
+
+def getAllMissions():
+    return db.mission.find({})
+
+
+def updateMission(id, updatedMission):
+    print('[UPDATE MISSION] ->', updatedMission)
+    query = dict(_id=id)
+    values = {"$set": dict(context=updatedMission)}
+
+    collection.update_one(query, values)
+    print('Updated', updatedMission)
+
+
+if __name__ == '__main__':
+
+    missionData = {"context": [{"asset": "GRT"}]}
+    createMission(missionData)
+
+    missions = list(getAllMissions())
+    print(missions)
+
+    missionId = missions[0]['_id']
+    missionData = [{"asset": "GRT"}, {"asset": "LINK"}, {"asset": "ALGO"}]
+    updateMission(missionId, missionData)
+
+    missions = list(getAllMissions())
+    print(missions)
