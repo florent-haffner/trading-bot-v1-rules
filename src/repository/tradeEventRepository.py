9902490519506c83136c4476f2e143013648b180
florent-haffner <florent.haffner@arkema.com>
2021-04-18 14:48:11 +0200

    update tradeEventRepository.py - migrating to Influx API v2

--- src/repository/tradeEventRepository.py
+++ src/repository/tradeEventRepository.py
@@ -78,40 +78,21 @@ def cleanTradeEvents():
 
 
 def initEnvironment():
-    tmp_db = 'tmp'
-    # print('InfluxDB HEALTH', __INFLUX_CLIENT.health())
+    delete_api = __INFLUX_CLIENT.delete_api()
+    # GET EVERYTHING
+    query = """
+        from (bucket:"ts_trade_event_dev")\
+        |> range(start: -2d)\
+        |> filter(fn: (r) => r._measurement == "tradeEvent")\
+        |> filter(fn: (r) => r.typeOfTrade == "buy" )
+    """
 
-    print('Current DBs', __INFLUX_CLIENT.get_list_database())
-    print('Creating :', tmp_db, ', temporary DB', '\n')
-    __INFLUX_CLIENT.create_database(tmp_db)
-
-    point = [
-        {
-            'measurement': 'tradeEvent',
-            'tags': {'typeOfTrade': 'buy'},
-            'fields': {
-                'asset': 'GRT',
-                'quantity': 32.,
-                'price': 32.
-            },
-            'time': (datetime.now() + timedelta(hours=2)).strftime(DATE_STR),
-        }
-    ]
-
-    insertTradeEvent(point)
-    point[0]['time'] = (datetime.now() + timedelta(hours=4)).strftime(DATE_STR)
-    insertTradeEvent(point)
-    getRecentEventByTypeAndAsset('GRT', 'buy')
-    getRecentEventByTypeAndAsset('GRT', 'sell')
-
-    __INFLUX_CLIENT.drop_database(tmp_db)
-    print('\nRemoving :', tmp_db, ', temporary DB')
-
-    getRecentEventByTypeAndAsset('GRT', 'buy')
-
-
-if __name__ == "__main__":
-    # initEnvironment()
+    result_db_request = __QUERY_API.query(org=__INFLUXDB_CURRENT_ORG, query=query)
+    results = []
+    for table in result_db_request:
+        for record in table.records:
+            results.append((record.get_field(), record.get_value()))
+    print('initial env', results)
 
     # INIT ENV
     point = [
@@ -127,39 +108,28 @@ if __name__ == "__main__":
     ]
     __WRITE_API.write(__CURRENT_BUCKET, __INFLUXDB_CURRENT_ORG, point)
 
-    # Update the point the write a new one
-    # point[0]['time'] = (datetime.now() + timedelta(hours=2)).strftime(DATE_STR)
-    __WRITE_API.write(__CURRENT_BUCKET, __INFLUXDB_CURRENT_ORG, point)
-
-
-    # GET EVERYTHING
-    query = ' from (bucket:"ts_trade_event_dev")\
-        |> range(start: -2d)\
-        |> filter(fn: (r) => r._measurement == "tradeEvent")\
-        |> filter(fn: (r) => r.typeOfTrade == "buy" )'
     result_db_request = __QUERY_API.query(org=__INFLUXDB_CURRENT_ORG, query=query)
-    print(result_db_request)
     results = []
     for table in result_db_request:
         for record in table.records:
-            print(record)
             results.append( (record.get_field(), record.get_value()) )
-    print(results)
+    print('result', results)
 
-    # REMOVE EVERYTHING
-    query = ' from (bucket:"ts_trade_event_dev")\
-        |> range(start: -2d)\
-        |> drop(fn: (column) => column =~ /.*/ )'
+    delete_api.delete('1970-01-01T00:00:00Z', datetime.today().strftime(DATE_STR),
+                      '_measurement=' + __MEASUREMENT_NAME,
+                      bucket=__CURRENT_BUCKET, org=__INFLUXDB_CURRENT_ORG)
 
-    # GET EVERYTHING
+
     result_db_request = __QUERY_API.query(org=__INFLUXDB_CURRENT_ORG, query=query)
-    print(result_db_request)
     results = []
     for table in result_db_request:
         for record in table.records:
-            print(record)
-            results.append( (record.get_field(), record.get_value()) )
-    print(results)
+            results.append((record.get_field(), record.get_value()))
+    print('Remove everything', results)
+
+
+if __name__ == "__main__":
+    initEnvironment()
 
     # cleanTradeEvents()
 
