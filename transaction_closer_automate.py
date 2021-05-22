2926d1121c42cf1615f0b1008d693495d07b6c7c
florent-haffner <florent.haffner@protonmail.com>
2021-05-22 11:43:54 +0200

    transaction_closer_automate.py - automated transaction closing

--- transaction_closer_automate.py
+++ transaction_closer_automate.py
@@ -1,14 +1,17 @@
 from datetime import datetime, timedelta
 
 from src.data.missionMongoUtils import getAllMissions
+from src.data.tradeEventUtils import insertTradeEvent
 from src.data.transactionMongoUtils import getAllTransaction, updateTransactionById
 from src.helpers.dateHelper import DATE_STR
-from src.helpers.params import MAXIMUM_PERCENTAGE_EUR
-from src.services.krakenTradeService import getAccountBalance, getLastPrice
+from src.services.krakenTradeService import getLastPrice
+from src.services.tradeEventService import generateTradeEventDTO
+from src.services.transactionService import updateToCompleteTransaction
+
 
 def close_everything():
     missions = getAllMissions()
-    nbr_asset_on_trade = len(missions[0]['context']['assets'])
+    interval = missions[0]['context']['interval']
     transactions = list(getAllTransaction())
     print('nbr transaction', len(transactions))
 
@@ -20,19 +23,38 @@ def close_everything():
         except KeyError:
             transactions_to_closed.append(transaction)
 
-    print('Nbr of transactions to closed', len(transactions_to_closed))
-    now = datetime.now()
-    previous_hour = now - timedelta(hours=1)
+    nbr_transactions_not_closed = len(transactions_to_closed)
+    print('Nbr of transactions not closed', nbr_transactions_not_closed)
+    if nbr_transactions_not_closed == 0:
+        print('No transactions to closed')
+        return
+
+    previous_hour = datetime.now() - timedelta(hours=1)
     for transaction in transactions_to_closed:
-        id = transaction['_id']
+        transactionId = transaction['_id']
         time = datetime.strptime(transaction['buy']['time'], DATE_STR)
         if time < previous_hour:
+            print('Closing', transactionId)
             last_price = getLastPrice(transaction['buy']['fields']['asset'], 'EUR')
             volume = transaction['buy']['fields']['quantity']
-            print('volume', volume, 'price', last_price, 'in euro:', last_price * volume)
+            # print('volume', volume, 'price', last_price, 'in euro:', last_price * volume)
+            type_of_trade = 'sell'
+            point = generateTradeEventDTO(type_of_trade=type_of_trade,
+                                          volume_to_buy=volume,
+                                          asset=transaction['buy']['fields']['asset'],
+                                          interval=interval,
+                                          price=last_price)
 
-        # # TODO -> wait to make sure before closing the transaction
-        # updateTransactionById(id, 'forced_closed', False)
+            print('Updating', transactionId, 'to complete transaction')
+            result = updateToCompleteTransaction(id=transactionId,
+                                                 key=type_of_trade,
+                                                 points=point)
+            if result:
+                del point['time']
+                insertTradeEvent([point])
+
+            # Finally update to make sure the transaction is closed
+            updateTransactionById(transactionId, 'forced_closed', True)
 
 
 if __name__ == '__main__':
