3740f81b1e95164f33da3a790af215859f32c051
florent-haffner <florent.haffner@protonmail.com>
2021-06-05 18:19:04 +0200

    tradeEventService.py - update OHLC trade index calculation

--- src/services/tradeEventService.py
+++ src/services/tradeEventService.py
@@ -136,11 +136,11 @@ def generate_graph_from_ohlc(data: list) -> list:
             print('\nNEW NODE', self.time)
             print('open', self.open)
             # low_p = self.open * self.low / 100
-            print('low', self.low, 'delta', self.open * self.low / 100)
+            print('low', self.low, 'delta', self.low * 100 / self.open)
             # high_p = self.open * self.high / 100
-            print('high', self.high, 'delta', self.open * self.high / 100)
+            print('high', self.high, 'delta', self.open * 100 / self.high)
             # close_p = self.open * self.close / 100
-            print('close', self.close, 'delta', self.open * self.close / 100)
+            print('close', self.close, 'delta', self.open * 100 / self.close)
 
     previous = {}
     for time_window in data:
